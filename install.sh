dc5a4ca638c7ad29114d6e238be3a6486badbf61
Otavio Alves Oliveira Costa <otavioalvescbp@gmail.com>
2026-04-15 14:39:19 -0300

    Update install script for dotfiles automation

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Script de automação de Dotfiles para o Phoenix
 
 echo "🚀 Iniciando a simbiose dos dotfiles..."
 
 # Criar a pasta .config caso ela não exista
 mkdir -p ~/.config
 
-# Remover a pasta nvim padrão caso ela exista (cuidado!)
+# Remover a pasta nvim padrão caso ela exista
 rm -rf ~/.config/nvim
 
 # Criar o link simbólico
